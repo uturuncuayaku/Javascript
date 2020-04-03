b157c291cdb6c2766f7e33dd4b794a8534ea536e
Andres <andtruj23@gmail.com>
2020-04-02 21:10:00 -0500

    Added Chapter 3

---
+++ Data_and_Algo/chpt3.js
@@ -0,0 +1,16 @@
+// Javascript Number System
+
+31-0
+001111100
+//front half, eight bits representing the exponential value and then most significant bit is the sign bit leading on the far left. The rest of the 32-bit floating point representation is the fractional 23-bit segment. Which consists of 
+01000000000000000000000000
+//this entire value is equal to .15625
+Math.floor   // rounds down
+Math.round   // rounds to nearest
+Math.ceiling // rounds up
+
+Math.floor(0.9); //0
+Math.round(1.1); //1
+Math.round(.5);  //1
+Math.ceil(0.1);  //1
+
